b86083dff8537a40d10e17617a73e1314b2ee38f
Karim <karim.rochat@heig-vd.ch>
2019-01-15 21:30:42 +0100

    J'enlève le A visible change

--- index.php
+++ index.php
@@ -77,7 +77,7 @@ $items = $db->query($selectQuery);
 
 <html>
   <head>
-    <title>A visible change</title>
+    <title>todolist</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
